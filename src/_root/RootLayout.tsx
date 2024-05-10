4cc3bfa0b5a3310b48cdaf050e05519c6cab3fad
Igors Pozdnakovs <ig.pozdnakovs@gmail.com>
2024-05-10 20:34:53 +0300

    chore: Update RootLayout to include shared components

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,14 @@
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import Bottombar from '@/components/shared/Bottombar'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+      <Bottombar />
+    </div>
   )
 }
 
