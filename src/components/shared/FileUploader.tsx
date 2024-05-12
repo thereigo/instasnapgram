00d2320952de08f84234cdf41760667b44269b9a
Igors Pozdnakovs <ig.pozdnakovs@gmail.com>
2024-05-12 21:54:01 +0300

    chore: Update dependencies and add react-dropzone@14.2.3

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,6 +1,28 @@
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+
 const FileUploader = () => {
+  const [fileUrl, setFilUrl] = useState('')
+  const onDrop = useCallback(acceptedFiles => {
+  // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div>FileUploader</div>
+    <div {...getRootProps()} className='flex flex-col flex-center bg-dark-3 rounded-xl cursor-pointer'>
+    <input {...getInputProps()} className='cursor-pointer'/>
+    {
+      fileUrl ? (
+        <div>
+          test 1
+        </div>
+      ) : (
+        <div>
+          test 2
+        </div>
+      )
+    }
+  </div>
   )
 }
 
