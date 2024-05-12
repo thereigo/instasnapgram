b9099170a26fcb01d7b5db3b0f34828fc5c332c1
Igors Pozdnakovs <ig.pozdnakovs@gmail.com>
2024-05-12 12:03:15 +0300

    chore: Update routing in App component and add new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
+
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
-import './globals.css'
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from "@/components/ui/toaster"
+import './globals.css'
 
 const App = () => {
   return (
@@ -19,6 +30,14 @@ const App = () => {
             {/* private routes */}
             <Route element={<RootLayout />}>
               <Route index element={<Home />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/saved" element={<Saved />} />
+              <Route path="/all-users" element={<AllUsers />} />
+              <Route path="/create-post" element={<CreatePost />} />
+              <Route path="/update-post/:id" element={<EditPost />} />
+              <Route path="/posts/:id" element={<PostDetails />} />
+              <Route path="/profile/:id/*" element={<Profile />} />
+              <Route path="/update-profile/:id" element={<UpdateProfile />} />
             </Route>
         </Routes>
         <Toaster />
