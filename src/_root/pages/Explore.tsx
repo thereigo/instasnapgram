54833b364da32488db5df612627c49dd1264b20c
thereigo <ig.pozdnakovs@gmail.com>
2024-06-03 17:17:54 +0300

    feat: Implement lazy loading for images in Explore and SearchResults components

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -7,6 +7,7 @@ import { useGetPosts, useSerchPosts } from "@/lib/react-query/queriesAndMutation
 import { useEffect, useState } from "react"
 import { useInView } from "react-intersection-observer";
 
+
 const Explore = () => {
   const { ref, inView } = useInView();
   const { data: posts, fetchNextPage, hasNextPage } = useGetPosts()
@@ -14,7 +15,7 @@ const Explore = () => {
   const [searchValue, setSearchValue] = useState('')
   const debounceValue = useDebounce(searchValue, 500)
   const { data: searchedPosts, isFetching: isSearchFetching } = useSerchPosts(debounceValue)
-
+  
   useEffect(() => {
     if (inView && !searchValue) {
       fetchNextPage();
@@ -62,13 +63,13 @@ const Explore = () => {
         {shouldShowSearchResults ? (
           <SearchResults
             isSearchFetching={isSearchFetching}
-            searchedPosts={searchedPosts}
+            searchedPosts={searchedPosts?.documents ?? []}
           />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
           posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
+            item && <GridPostList key={`page-${index}`} posts={item.documents} />
           ))
         )}
       </div>
--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -11,8 +11,8 @@ type SearchResultProps = {
 const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
   if (isSearchFetching) {
     return <Loader />;
-  } else if (searchedPosts && searchedPosts.documents.length > 0) {
-    return <GridPostList posts={searchedPosts.documents} />;
+  } else if (searchedPosts && searchedPosts.length > 0) {
+    return <GridPostList posts={searchedPosts} />;
   } else {
     return (
       <p className="text-light-4 mt-10 text-center w-full">No results found</p>
